b2d1a523e247665d53beea45cdd445b0f8fcf081
Brent Vatne <brentvatne@gmail.com>
2016-11-12 13:07:27 -0800

    Use the node_modules versions of the assets

--- main.js
+++ main.js
@@ -19,17 +19,17 @@ class App extends React.Component {
 
   async componentWillMount() {
     await Font.loadAsync({
-      'Rubik-Black': require('./assets/fonts/Rubik-Black.ttf'),
-      'Rubik-BlackItalic': require('./assets/fonts/Rubik-BlackItalic.ttf'),
-      'Rubik-Bold': require('./assets/fonts/Rubik-Bold.ttf'),
-      'Rubik-BoldItalic': require('./assets/fonts/Rubik-BoldItalic.ttf'),
-      'Rubik-Italic': require('./assets/fonts/Rubik-Italic.ttf'),
-      'Rubik-Light': require('./assets/fonts/Rubik-Light.ttf'),
-      'Rubik-LightItalic': require('./assets/fonts/Rubik-LightItalic.ttf'),
-      'Rubik-Medium': require('./assets/fonts/Rubik-Medium.ttf'),
-      'Rubik-MediumItalic': require('./assets/fonts/Rubik-MediumItalic.ttf'),
-      'Rubik-Regular': require('./assets/fonts/Rubik-Regular.ttf'),
-      'rubicon-icon-font': require('./assets/fonts/rubicon-icon-font.ttf'),
+      'Rubik-Black': require('./node_modules/@shoutem/ui/fonts/Rubik-Black.ttf'),
+      'Rubik-BlackItalic': require('./node_modules/@shoutem/ui/fonts/Rubik-BlackItalic.ttf'),
+      'Rubik-Bold': require('./node_modules/@shoutem/ui/fonts/Rubik-Bold.ttf'),
+      'Rubik-BoldItalic': require('./node_modules/@shoutem/ui/fonts/Rubik-BoldItalic.ttf'),
+      'Rubik-Italic': require('./node_modules/@shoutem/ui/fonts/Rubik-Italic.ttf'),
+      'Rubik-Light': require('./node_modules/@shoutem/ui/fonts/Rubik-Light.ttf'),
+      'Rubik-LightItalic': require('./node_modules/@shoutem/ui/fonts/Rubik-LightItalic.ttf'),
+      'Rubik-Medium': require('./node_modules/@shoutem/ui/fonts/Rubik-Medium.ttf'),
+      'Rubik-MediumItalic': require('./node_modules/@shoutem/ui/fonts/Rubik-MediumItalic.ttf'),
+      'Rubik-Regular': require('./node_modules/@shoutem/ui/fonts/Rubik-Regular.ttf'),
+      'rubicon-icon-font': require('./node_modules/@shoutem/ui/fonts/rubicon-icon-font.ttf'),
     });
 
     this.setState({fontsAreLoaded: true});
